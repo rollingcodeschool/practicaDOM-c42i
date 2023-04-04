eedd209819bac97e41c0f4e3a9d8fd0427fd05e2
Emilse Arias <emilsearias@gmail.com>
2023-04-04 18:22:50 -0300

    agregamos un nuevo parrafo al dom

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+let botonVerMas = document.getElementById('btnVerMas');
+// agregar un manejador de eventos
+botonVerMas.addEventListener('click', verMas);
+// si tengo que enviar argumentos
+// botonVerMas.addEventListener('click', ()=>{ verMas(param1,param2)});
+
 function cambiarTitulo(){
     console.log('Desde cambiar Titulo');
     // traer el h1 del html
@@ -11,4 +17,30 @@ function cambiarTitulo(){
     titulo.className = 'text-primary';
     titulo.innerHTML = 'Nuevo titulo desde <b>JS</b>';
     // titulo.innerText = 'Nuevo titulo desde JS';
+}
+
+function verMas(){
+    console.log('Desde la funcion ver mas')
+    //traemos el section padre
+    let seccionPadre = document.querySelector('#contenedorPadre');
+    //crear el nuevo elemento
+    let parrafo = document.createElement('p');
+    console.log(parrafo) 
+    parrafo.innerHTML = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, ipsa
+    quam aut odit maxime, eligendi dolor nesciunt pariatur nisi beatae
+    dolore, culpa iste? Quod minus, tenetur incidunt ducimus molestias
+    neque vitae maiores ratione ea perferendis facere nulla, itaque
+    deserunt, totam quas officia. Voluptate cupiditate eveniet, est
+    numquam beatae neque, veritatis debitis ea iusto magni, sapiente totam
+    architecto assumenda. Quasi tempore tenetur, dolor beatae voluptatibus
+    maiores. Unde et, quod minus quam eos debitis sapiente consequatur
+    laudantium explicabo in facilis vitae veniam neque nesciunt animi
+    reprehenderit qui, recusandae quisquam nam tenetur? Iure, ad! Maxime
+    adipisci neque incidunt omnis, rerum corrupti sequi eveniet aspernatur
+    in velit minima, minus quidem iusto ex autem tempora laudantium?`;
+    parrafo.className = 'lead';
+    //insertar el nodo hijo
+    // seccionPadre.appendChild(parrafo);//agrega el nodo al final
+    // seccionPadre.prepend(parrafo);
+    seccionPadre.insertBefore(parrafo, botonVerMas);
 }
